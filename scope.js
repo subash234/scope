eff548b057aded60495f7d9e854b3047c8efcdf4
subash babu <subash.vutukuri@gmail.com>
2021-09-16 12:59:01 +0530

    Initial add of the files

---
+++ scope.js
@@ -0,0 +1,24 @@
+console.log("inside the scope.js file");
+// let d=10000;
+// const e=1000000;
+
+  
+//     // console.log("inside the block ");
+//     // var a=100;
+//     // console.log(a);
+//     {
+//     var a=10;
+//     let b=100;
+//     const c=1000;
+//     }
+//     console.log(a,b,c,e,d);
+
+let a=10;
+
+function  sum(){
+     var a=100;
+    console.log(a);
+}
+sum();
+
+
