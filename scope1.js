2e6794183368cbfa54c76ec74829c1a76cd671ac
subash babu <subash.vutukuri@gmail.com>
2021-09-18 13:37:42 +0530

    shjs

--- scope1.js
+++ scope1.js
@@ -1,30 +1,54 @@
-console.log("inside the scope.js");
-// let d=10000;
-// const e=1000000;
+// // console.log("inside the scope.js");
+// // // let d=10000;
+// // // const e=1000000;
 
   
-//     // console.log("inside the block ");
-//     // var a=100;
-//     // console.log(a);
-//     {
-//     var a=10;
-//     let b=100;
-//     const c=1000;
-//     }
-//     console.log(a,b,c,e,d);
+// // //     // console.log("inside the block ");
+// // //     // var a=100;
+// // //     // console.log(a);
+// // //     {
+// // //     var a=10;
+// // //     let b=100;
+// // //     const c=1000;
+// // //     }
+// // //     console.log(a,b,c,e,d);
+// let a=10;
 
-let a=10;
+// function  sum(){
+//      var a=100;
+//     console.log(a);
+// }
+// sum();
 
-function  sum(){
-     var a=100;
-    console.log(a);
-}
-sum();
+// console.log("in the master branch");
+// console.log('connected to remote branch from local branch')
+// console.log("workinng on the staging area");
 
-console.log("in the master branch");
-console.log('connected to remote branch from local branch')
-console.log("workinng on the staging area");
+// console.log('connected to remote branch from local branch');
 
-console.log('connected to remote branch from local branch');
+// // let a=10;
 
+// // function  sum(){
+// //      var a=100;
+// //     console.log(a);
+// // }
+// // sum();
+// // console.log("in the master branch");
+// // console.log('connected to remote branch from local branch')
+// // console.log("workinng on the staging area")
 
+// // let a=10;
+
+// // function  sum(){
+// //      var a=100;
+// //     console.log(a);
+// // }
+// // sum();
+// // console.log("in the master branch");
+// // console.log('connected to remote branch from local branch')
+// // console.log("workinng on the staging area")
+
+// //first class functions in js
+// function a(){
+//     console.log("inside the a function");
+// }
